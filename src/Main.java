17b3c1175038cf77eb6f167e7333fd05e3eaab93
kozyulichev <kozulichev@gmail.com>
2021-02-27 14:14:28 +0300

    Было переделанно на массив и исправлен счетчик

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         Cat cat = new Cat("Чупа", 10, 2, 10, 10);
-        Cat cat1 = new Cat("Улюка",100,0,2,98);
+        Cat cat1 = new Cat("Улюка", 100, 0, 2, 98);
         Dog dog = new Dog("Киану", 470, 23, 10, 500);
-        Animal[] animals = {cat,cat1, dog};
-        for (int i = 0; i < animals.length; i++) {
-            animals[i].run();
-            animals[i].swimm();
-            System.out.println("Количество созданных животных = "+(i+1));
+        Dog dog1 = new Dog("Лиза",20,10,5,1);
+        Animal[] animals = {cat, cat1, dog,dog1};
+        for (Animal animal : animals) {
+            animal.run();
+            animal.swimm();
+            System.out.println();
         }
+        System.out.println("Количество созданных животных = " + animals.length);
     }
 
 
